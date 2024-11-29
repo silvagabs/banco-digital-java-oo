a6e030b09ee546aaa9a0ab63c0730395994ee550
Gabriel Silva <gabelimaa2@gmail.com>
2024-11-29 11:25:45 +0000

    Transferencia

--- Main.java
+++ Main.java
@@ -7,8 +7,8 @@ public class Main {
 		Conta cc = new ContaCorrente(venilton);
 		Conta poupanca = new ContaPoupanca(venilton);
 
-		cc.depositar(500);
-		cc.transferir(50, poupanca);
+		cc.depositar(150);
+		cc.transferir(100, poupanca);
 		
 		cc.imprimirExtrato();
 		poupanca.imprimirExtrato();
@@ -16,18 +16,26 @@ public class Main {
 		Cliente gabriel = new Cliente();
 		gabriel.setNome("Gabriel");
 
+
 		Conta cc2= new ContaCorrente(gabriel);
 		Conta poupanca2= new ContaPoupanca(gabriel);
 
 		cc2.depositar(400);
 		cc2.transferir(100, poupanca2);
-		cc2.transferir(100, cc);
+		cc2.transferir(40, cc);
 
 	   
 		cc2.imprimirExtrato();
 		poupanca2.imprimirExtrato();
 
 		cc.imprimirExtrato();
+
+		if (cc != null) {
+			System.out.println("Venilton recebeu uma transferência!");
+			
+		}
+
+		
 
 
 
